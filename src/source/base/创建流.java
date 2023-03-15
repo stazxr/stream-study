ad6fea2244edb0ed078d83509064707eddbe826a
SunTao <1027353579@qq.com>
2023-03-15 11:02:10 +0800

    流创建方式学习

--- src/source/base/创建流.java
+++ src/source/base/创建流.java
@@ -10,6 +10,8 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
+import static java.util.regex.Pattern.*;
+
 public class 创建流 {
     public static void main(String[] args) throws Exception {
         // 集合：使用 stream() 和 parallelStream() 方法
@@ -39,7 +41,7 @@ public class 创建流 {
     }
 
     private static void create05() {
-        Pattern pattern = Pattern.compile(",");
+        Pattern pattern = compile(",");
         Stream<String> stringStream = pattern.splitAsStream("a,b,c,d");
         stringStream.forEach(System.out::println);
     }
@@ -52,31 +54,44 @@ public class 创建流 {
     }
 
     private static void create03() {
-        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5, 6);
+        // 值创建，它可以接收任意数量的参数
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
         stream.forEach(System.out::println);
+        System.out.println("===========");
 
-        // 0 2 4 6 8 10
-        Stream<Integer> stream2 = Stream.iterate(0, (x) -> x + 2).limit(6);
+        // 无限流（需要使用limit截断），iterate
+        Stream<Integer> stream2 = Stream.iterate(0, i -> i + 1).limit(5);
         stream2.forEach(System.out::println);
+        System.out.println("===========");
 
-        Stream<Double> stream3 = Stream.generate(Math::random).limit(2);
+        // 无限流（需要使用limit截断），generate
+        Stream<Double> stream3 = Stream.generate(Math::random).limit(5);
         stream3.forEach(System.out::println);
     }
 
     private static void create02() {
-        Integer[] nums = new Integer[10];
+        Integer[] nums = new Integer[]{1, 2, 3, 4, 5};
         Stream<Integer> stream = Arrays.stream(nums);
         stream.forEach(System.out::println);
+        System.out.println("===========");
+        Stream<Integer> stream1 = Arrays.stream(nums, 1, 3);
+        stream1.forEach(System.out::println);
     }
 
     private static void create01() {
         List<String> list = new ArrayList<>();
+        list.add("1");
+        list.add("2");
+        list.add("3");
+        list.add("4");
+        list.add("5");
 
-        // 获取一个顺序流
+        // 获取一个串行流，打印结果：1、2、3、4、5
         Stream<String> stream = list.stream();
         stream.forEach(System.out::println);
+        System.out.println("===========");
 
-        // 获取一个并行流
+        // 获取一个并行流，打印结果（非有序）：3、4、5、2、1
         Stream<String> parallelStream = list.parallelStream();
         parallelStream.forEach(System.out::println);
     }
